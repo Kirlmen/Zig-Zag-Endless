122ed3f22a4dc4306ac406ae45f909f49afaf144
Kirlmen <kirlmen0764@gmail.com>
2022-01-10 19:05:25 +0300

    movespeed

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,13 +5,14 @@ using RDG;
 
 public class CarController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    public static CarController Instance;
+    [SerializeField] public float moveSpeed = 8f;
     private bool movingLeft = true;
     //private bool firstInput = true;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-
+        Instance = this;
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -24,17 +24,21 @@ public class PlatformScript : MonoBehaviour
         else if (GameManager.Instance.score < 2500 && GameManager.Instance.score > 1000)
         {
             dropTimer = 1f;
+            CarController.Instance.moveSpeed = 9f;
         }
         else if (GameManager.Instance.score < 5000 && GameManager.Instance.score > 2500)
         {
+            CarController.Instance.moveSpeed = 10f;
             dropTimer = 0.6f;
         }
         else if (GameManager.Instance.score < 8000 && GameManager.Instance.score > 5000)
         {
+            CarController.Instance.moveSpeed = 11f;
             dropTimer = 0.3f;
         }
         else if (GameManager.Instance.score < 15000 && GameManager.Instance.score > 8000)
         {
+            CarController.Instance.moveSpeed = 12f;
             dropTimer = 0.2f;
         }
 
